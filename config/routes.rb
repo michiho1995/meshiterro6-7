743d531697624ffe08b92fb7dc2f8f3d0c4f2b8a
EC2 Default User <ec2-user@ip-172-31-76-216.ec2.internal>
2020-10-18 12:16:45 +0000

    [finish]6-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   root 'post_images#index'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
